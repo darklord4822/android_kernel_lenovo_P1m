50349cc910e59bf6c60e173f280a7b267a7e30e7
darklord4822 <darklord4722@gmail.com>
2017-08-03 13:17:53 +0000

    mediatek: fix flashlight

--- drivers/misc/mediatek/imgsensor/src/mt6735m/camera_hw/kd_camera_hw.h
+++ drivers/misc/mediatek/imgsensor/src/mt6735m/camera_hw/kd_camera_hw.h
@@ -1,6 +1,7 @@
 #ifndef _KD_CAMERA_HW_H_
 #define _KD_CAMERA_HW_H_
 
+
 #include <linux/types.h>
 #include "kd_camera_typedef.h"
 
@@ -21,10 +22,14 @@
 /* Digital for Sub */
 #define SUB_CAMERA_POWER_VCAM_D     PMIC_APP_SUB_CAMERA_POWER_D
 
+
 /* FIXME, should defined in DCT tool */
+
 /* Main sensor */
-#define CAMERA_CMRST_PIN            GPIO_CAMERA_CMRST_PIN
-#define CAMERA_CMRST_PIN_M_GPIO     GPIO_CAMERA_CMRST_PIN_M_GPIO
+    /* Common phone's reset pin uses extension GPIO10 of mt6306 */
+    #define CAMERA_CMRST_PIN            GPIO_CAMERA_CMRST_PIN
+    #define CAMERA_CMRST_PIN_M_GPIO     GPIO_CAMERA_CMRST_PIN_M_GPIO
+
 
 #define CAMERA_CMPDN_PIN            GPIO_CAMERA_CMPDN_PIN
 #define CAMERA_CMPDN_PIN_M_GPIO     GPIO_CAMERA_CMPDN_PIN_M_GPIO
@@ -56,6 +61,11 @@
 #define GPIO_OUT_ONE 1
 #define GPIO_OUT_ZERO 0
 
+
+
+int mtkcam_gpio_set(int PinIdx, int PwrType, int Val);
+int mtkcam_gpio_init(struct platform_device *pdev);
+
 #endif /* End of #if defined CONFIG_MTK_LEGACY */
 
 
@@ -64,7 +74,7 @@ typedef enum KD_REGULATOR_TYPE_TAG {
 	VCAMD,
 	VCAMIO,
 	VCAMAF,
-	SUB_VCAMD
+	SUB_VCAMD,
 } KD_REGULATOR_TYPE_T;
 
 typedef enum {
@@ -72,66 +82,35 @@ typedef enum {
 	CAMRST,
 	CAM1PDN,
 	CAM1RST,
-	CAMDLDO,
-	CAMALDO
+	CAMLDO
 } CAMPowerType;
+
+//yinyapeng add ==>
+ typedef enum {
+ 		MAIN_NOCAMERA,
+		MAIN_S5K5E2,
+		MAIN_GC5005,
+		MAIN_HI553,
+} UsedMainCameraType; 
+
+ typedef enum {
+ 		SUB_NOCAMERA,
+		SUB_GC2355,
+		SUB_GC2365,
+		SUB_SP2509,
+} UsedSubCameraType; 
+ 
+
+
+//<===end 
+
+extern bool _hwPowerDown(KD_REGULATOR_TYPE_T type);
+extern bool _hwPowerOn(KD_REGULATOR_TYPE_T type, int powerVolt);
 
 extern void ISP_MCLK1_EN(BOOL En);
 extern void ISP_MCLK2_EN(BOOL En);
 
 
-extern bool _hwPowerDown(KD_REGULATOR_TYPE_T type);
-extern bool _hwPowerOn(KD_REGULATOR_TYPE_T type, int powerVolt);
 
-int mtkcam_gpio_set(int PinIdx, int PwrType, int Val);
-int mtkcam_gpio_init(struct platform_device *pdev);
 
 #endif
-
-#define VOL_2800 2800000
-#define VOL_1800 1800000
-#define VOL_1500 1500000
-#define VOL_1200 1200000
-#define VOL_1000 1000000
-
-typedef enum{
-    VDD_None = 0,
-    SensorId = 0xf1,
-    SensorMCLK = 0xf2,
-    PDN = 0xf3,
-    RST = 0xf4,
-    AVDD = 0xf5,
-    DVDD = 0xf6,
-    DOVDD = 0xf7,
-    AFVDD = 0xf8
-}PowerType;
-
-typedef enum{
-	MAIN_SENSOR = 0,
-	SUB_SENSOR = 1,
-	MAIN_2_SENSOR = 2,
-	Mclk1 = 0xf1,
-	Mclk2 = 0xf2,
-    Vol_Low = 0xf3,
-    Vol_High = 0xf4,
-    Vol_1000 = VOL_1000,
-    Vol_1200 = VOL_1200,
-    Vol_1500 = VOL_1500,
-    Vol_1800 = VOL_1800,
-    Vol_2800 = VOL_2800
-}Voltage;
-
-typedef struct{
-    PowerType PowerType;
-    Voltage Voltage;
-    u32 Delay;
-}PowerInformation;
-
-typedef struct{
-    char* SensorName;
-    PowerInformation PowerInfo[15];
-}PowerSequence;
-
-typedef struct{
-    PowerSequence PowerSeq[16];
-}PowerUp;
